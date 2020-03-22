fcb7126dab579c7b77006658a8c93f229f6b8d2e
Jakub Jastrabík <jakub.jastrabik@lastmile.sk>
2020-03-22 23:50:29 +0100

    Add limit per page to 1000 rows. Add metric for aps

--- smartzone_exporter.py
+++ smartzone_exporter.py
@@ -77,7 +77,7 @@ class SmartZoneCollector():
             r = requests.post('{}/wsg/api/public/v9_0/{}'.format(self._target, api_path), json=raw,
                               headers=self._headers, verify=self._insecure)
         else:
-            r = requests.get('{}/wsg/api/public/v9_0/{}'.format(self._target, api_path), headers=self._headers,
+            r = requests.get('{}/wsg/api/public/v9_0/{}'.format(self._target, api_path+'?listSize=1000'), headers=self._headers,
                              verify=self._insecure)
         result = json.loads(r.text)
         return result
@@ -355,6 +355,53 @@ class SmartZoneCollector():
                                   labels=["zone_id", "ap_mame", "ap_mac", "serial"])
         }
 
+        ap_metrics = {
+            'model':
+                GaugeMetricFamily('smartzone_ap_model',
+                                  'SmartZone AP model',
+                                  labels=["ap_mac", "model"]),
+            'version':
+                GaugeMetricFamily('smartzone_ap_version',
+                                  'SmartZone AP version',
+                                  labels=["ap_mac", "version"]),
+            'description':
+                GaugeMetricFamily('smartzone_ap_description',
+                                  'SmartZone AP description',
+                                  labels=["ap_mac", "description"]),
+            'zoneId':
+                GaugeMetricFamily('smartzone_ap_zoneId',
+                                  'SmartZone AP zone id',
+                                  labels=["ap_mac", "zoneId"]),
+            'connectionState':
+                GaugeMetricFamily('smartzone_ap_connectionState',
+                                  'SmartZone AP connection state',
+                                  labels=["ap_mac", "connectionState"]),
+            'wifi50Channel':
+                GaugeMetricFamily('smartzone_ap_wifi50Channel',
+                                  'SmartZone AP 5GHz channel number',
+                                  labels=["ap_mac"]),
+            'wifi24Channel':
+                GaugeMetricFamily('smartzone_ap_wifi24Channel',
+                                  'SmartZone AP 2.4GHz channel number',
+                                  labels=["ap_mac"]),
+            'approvedTime':
+                GaugeMetricFamily('smartzone_ap_approvedTime',
+                                  'SmartZone AP approved time',
+                                  labels=["ap_mac"]),
+            'lastSeenTime':
+                GaugeMetricFamily('smartzone_ap_lastSeenTime',
+                                  'SmartZone AP last seen time',
+                                  labels=["ap_mac"]),
+            'uptime':
+                GaugeMetricFamily('smartzone_ap_uptime',
+                                  'SmartZone AP uptime',
+                                  labels=["ap_mac"]),
+            'clientCount':
+                GaugeMetricFamily('smartzone_ap_clientCount',
+                                  'SmartZone AP client count',
+                                  labels=["ap_mac"])
+        }
+
         ap_summary_list = {
             'location':
                 GaugeMetricFamily('smartzone_aps_location',
@@ -485,16 +532,32 @@ class SmartZoneCollector():
         # - Grab the zone ID for labeling purposes
         # - For each APs, get mac, zoneID, apGroupIdm, name, lanPortSize
 
+        ap_glob_mac = []
         for ap in self.get_metrics(ap_list, 'aps')['list']:
             zone_id = ap['zoneId']
             ap_mame = ap['name']
             ap_mac = ap['mac']
+            ap_glob_mac.append(ap_mac)
             for s in self._statuses:
                 # Export a dummy value for string-only metrics
                 extra = ap[s]
                 ap_list[s].add_metric([zone_id, ap_mame, ap_mac, extra], 1)
 
         for m in ap_list.values():
+            yield m
+
+        for ap_mac in ap_glob_mac:
+            path = 'aps/' + ap_mac + '/operational/summary'
+            ap_detail = self.get_metrics(ap_metrics, path)
+            for d in self._statuses:
+                if d == 'description' or d == 'version' or d == 'model' or d == 'zoneId' or d == 'connectionState':
+                    extra = ap_detail[d]
+                    ap_metrics[d].add_metric([ap_mac, extra], 1)
+                else:
+
+                    ap_metrics[d].add_metric([ap_mac], ap_detail.get(d))
+
+        for m in ap_metrics.values():
             yield m
 
         # Get APs summary information
